2862a20ab9751a9c430c65c2ed27944416e836e1
Lars Muehlenstaedt <muehlenstaedt.lars@gmail.com>
2020-09-14 20:04:53 +0200

    change button to delete

--- keyboards/ergodone/keymaps/mhlnstdt/keymap.c
+++ keyboards/ergodone/keymaps/mhlnstdt/keymap.c
@@ -43,18 +43,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_CAPS,        KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
         KC_LSFT,        KC_Y,         KC_X,   KC_C,   KC_V,   KC_B,   KC_QUOT,
         KC_LCTL ,KC_LGUI,     KC_LALT,  TT(SYMB), TT(MDIA),
-                                                              KC_HOME,  KC_END,
-                                                              KC_PSCR,
-                                                              KC_SPC,KC_ENT,KC_END,
+                                                              KC_HOME,KC_END,
+                                                                      KC_PSCR,
+                                                        KC_SPC,KC_ENT,KC_DEL,
         // right hand
              KC_MINS,     KC_6,   KC_7,  KC_8,   KC_9,   KC_0,             KC_BSPC,
              KC_RBRC,     KC_Z,   KC_U,  KC_I,   KC_O,   KC_P,             KC_ENT,
                           KC_H,   KC_J,  KC_K,   KC_L,   KC_SCLN ,         KC_BSLS,
              KC_EQL,      KC_N,   KC_M,  KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
-                                  KC_NO, KC_LEFT,KC_UP,  KC_DOWN,            KC_RGHT,
-                                                                KC_INS,         KC_ESC,
-                                                                KC_PGUP,
-                                                                KC_PGDN,KC_ENT, KC_SPC
+                                  KC_NO, KC_LEFT,KC_UP,  KC_DOWN,          KC_RGHT,
+    KC_INS,  KC_ESC,
+    KC_PGUP,
+    KC_PGDN, KC_ENT, KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
